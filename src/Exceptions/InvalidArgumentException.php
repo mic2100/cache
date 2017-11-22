185c07df3514368be58e561177100aaf5820c626
Michael Bardsley <mic.bardsley@outlook.com>
2017-11-22 22:15:11 +0000

    Added the excpetion classes

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mic2100\Cache\Exceptions;
+
+class InvalidArgumentException extends \InvalidArgumentException implements \Psr\SimpleCache\InvalidArgumentException
+{
+
+}
---
+++ src/Exceptions/NotFoundKeyException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mic2100\Cache\Exceptions;
+
+class NotFoundKeyException extends \Exception
+{
+
+}
